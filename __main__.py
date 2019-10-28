f4a616ef9bdfcb2e09cd163bec6acff690de2c4c
Ellie Mae Galyean <elliemaegalyean@gmail.com>
2019-10-27 21:19:32 -0400

    added --subreddit option

--- __main__.py
+++ __main__.py
@@ -7,6 +7,7 @@ import os
 def get_args():
     p = argparse.ArgumentParser()
     p.add_argument('path', help='the folder where your rice files are located')
+    p.add_argument('--subreddit', help='name of the subreddit you wanna post to, deafault is \'unixporn\'', default='unixporn', nargs='?')
     p.add_argument('-g', '--generate', help='uses the katfetch module to generte basic details', action='store_true')
     return p.parse_args()
 
@@ -18,16 +19,16 @@ def upload_scrots(directory : str, title : str) -> str:
     """
     return imgurUploader.main([ f'{directory}/{i}' for i in os.listdir(directory) if i.endswith('.png')], title)[1]
 
-def main(folder : str, gen=False) -> str:
+def main(folder : str, subreddit : str, gen=False) -> str:
     credentials_file = f'{os.environ.get("HOME")}/.config/unixpornposter/redditCreds'
     with open(credentials_file) as f:
         credentials = eval(f.read())
     title, comment = riceDetails.main(folder, gen=gen)
     link = upload_scrots(folder, title)
-    submission = redditpost.main(title, 'unixporn', credentials, url=link)
+    submission = redditpost.main(title, subreddit, credentials, url=link)
     submission.reply(comment)
     return submission.permalink
 
 if __name__ == '__main__':
     a = get_args()
-    print(f'https://reddit.com{main(a.path, gen=a.generate)}')
+    print(f'https://reddit.com{main(a.path, a.subreddit, gen=a.generate)}')
